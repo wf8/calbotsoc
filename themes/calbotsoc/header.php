afdc583804bcb9d9d2230c9b27436f0fbc21097c
wf <willfreyman@gmail.com>
2014-03-16 14:24:34 -0700

    changed title for all pages

--- themes/calbotsoc/header.php
+++ themes/calbotsoc/header.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		
-		<title><?php wp_title(''); ?></title>
+		<title>Califorania Botanical Society</title>
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
